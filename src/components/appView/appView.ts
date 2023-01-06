f0b0294daf29edbba586692059b6a01ba723fb79
Lex <pineapplepear55@gmail.com>
2023-01-07 00:46:42 +0300

    fix: fix all bugs from tasklist

--- src/components/appView/appView.ts
+++ src/components/appView/appView.ts
@@ -21,10 +21,13 @@ class AppView {
     this.catalog = new Catalog();
   }
 
+
+  // Show product details
   showProductDetails(data: Types.Product) {
     this.productDetails.drawProduct(data);
   }
 
+  // Create toggle
   createToggle() {
     const toggleQuery = new URLSearchParams(window.location.search);
     const toggleBtn = <HTMLDivElement>document.querySelector('.display-icon');
@@ -60,6 +63,7 @@ class AppView {
     }
   }
 
+  // Sort dropdown
   createDropdown() {
     const dropdownSort = document.querySelector('.sort-dropdown');
     const dropdownMenu = document.querySelector('.sort-dropdown-items');
@@ -68,6 +72,7 @@ class AppView {
     });
   }
 
+  // Retrieve categories from data --> draw
   createCheckFilters(data: Types.Product[], type: string) {
     const filterDiv = <HTMLDivElement>document.querySelector(`.${type}-filters`);
     let filters: string[] = [];
@@ -125,6 +130,20 @@ class AppView {
     this.catalog.drawSliderFilter(discount, 'discount');
   }
 
+  // Pagination
+
+  initPages(filteredArr: Types.Product[], pagesCount: number) {
+    const catalogPages = <HTMLDivElement>document.querySelector('.catalog-pages');
+    if (catalogPages) {
+      if (pagesCount <= 0) {
+        catalogPages.style.display = 'none';
+      } else {
+        catalogPages.style.display = 'flex';
+        this.createPages(filteredArr, pagesCount);
+      }
+    }
+  }
+
   initPagesandFilter(filteredArr: Types.Product[], filtersObj: Types.IFilters) {
     const storagedItems = localStorage.getItem('onlineStoreCart112547');
     if (storagedItems) this.cart.cartItems = JSON.parse(storagedItems);
@@ -139,6 +158,82 @@ class AppView {
     }
   }
 
+  createPages(filteredArr: Types.Product[], pagesCount: number) {
+    const catalogPages = document.querySelector('.catalog-pages');
+    // const pagesWrapper = document.querySelector('.pages-wrapper');
+    const pagesWrapper = document.createElement('div');
+    pagesWrapper?.classList.add('pages-wrapper');
+    const pageNext = document.createElement('img');
+    const pagePrev = document.createElement('img');
+    pageNext.src = '../../assets/icons/btn-next.svg';
+    pagePrev.src = '../../assets/icons/btn-prev.svg';
+    // const pageNext = document.querySelector('.page-next');
+    // const pagePrev = document.querySelector('.page-prev');
+    pageNext.classList.add('btn-page');
+    pagePrev.classList.add('btn-page');
+    pageNext.classList.add('page-next');
+    pagePrev.classList.add('page-prev');
+
+    if (catalogPages) {
+      catalogPages.innerHTML = '';
+    }
+    catalogPages?.append(pagePrev);
+    const pagesArr: HTMLDivElement[] = [];
+    // pagesArr.push(pageNext)
+    for (let i = 0; i < pagesCount; i++) {
+      pagesArr.push(document.createElement('p'));
+      pagesArr[i].className = 'catalog__page';
+      pagesArr[i].className = 'page-idx';
+      pagesArr[i].textContent = String(i + 1);
+      pagesWrapper?.append(pagesArr[i]);
+      pagesArr[i].addEventListener('click', () => {
+        this.goToPage(filteredArr, pagesArr, i);
+      });
+    }
+    catalogPages?.append(pagesWrapper, pageNext);
+    pagesArr[0].classList.add('page-idx--active');
+    if (pageNext && pageNext.getAttribute('listener') !== 'true') {
+      pageNext.addEventListener('click', () => {
+        this.goToPage(filteredArr, pagesArr, this.findPageIdx(pagesArr) + 1);
+      });
+      pageNext.setAttribute('listener', 'true');
+    }
+
+    if (pagePrev && pagePrev.getAttribute('listener') !== 'true') {
+      pagePrev.addEventListener('click', () => {
+        if (this.findPageIdx(pagesArr) - 1 >= 0) {
+          this.goToPage(filteredArr, pagesArr, this.findPageIdx(pagesArr) - 1);
+        } 
+        console.log('gotopagePrev', this.findPageIdx(pagesArr) - 1);
+      });
+      pagePrev.setAttribute('listener', 'true');
+    }
+  }
+
+  goToPage(filteredArr: Types.Product[], pagesArr: HTMLDivElement[], idx: number) {
+    console.log(filteredArr);
+    
+    const catalogDiv: HTMLDivElement | null = document.querySelector('.cards-wrapper');
+    if (idx >= 0 && idx < pagesArr.length) {
+      for (let i = 0; i < pagesArr.length; i++) {
+        pagesArr[i].classList.remove('page-idx--active');
+      }
+      pagesArr[idx].classList.add('page-idx--active');
+      if (catalogDiv) {
+        this.createCatalog(filteredArr, catalogDiv, idx);
+      }
+    }
+  }
+
+  findPageIdx(pagesArr: HTMLDivElement[]) {
+    for (let i = 0; i < pagesArr.length; i++) {
+      if (pagesArr[i].classList.contains('page-idx--active')) return i;
+    }
+    return 0;
+  }
+
+
+  // Catalog methods
   createCatalog(filteredArr: Types.Product[], catalogDiv: HTMLDivElement, page: number) {
     const startIdx = page * PAGINATION_COUNT;
     let endIdx = filteredArr.length >= PAGINATION_COUNT ? startIdx + PAGINATION_COUNT : filteredArr.length;
@@ -161,18 +256,6 @@ class AppView {
     }
   }
 
-  initPages(filteredArr: Types.Product[], pagesCount: number) {
-    const catalogPages = <HTMLDivElement>document.querySelector('.catalog-pages');
-    if (catalogPages) {
-      if (pagesCount <= 0) {
-        catalogPages.style.display = 'none';
-      } else {
-        catalogPages.style.display = 'flex';
-        this.createPages(filteredArr, pagesCount);
-      }
-    }
-  }
-
   createSortedCatalog(filteredArr: Types.Product[]) {
     const catalogDiv: HTMLDivElement | null = document.querySelector('.cards-wrapper');
     if (catalogDiv) {
@@ -185,6 +268,8 @@ class AppView {
   initSorting(filteredArr: Types.Product[]) {
     const sortParams = new URLSearchParams(window.location.search);
     this.sortArrayInitial(filteredArr, sortParams);
+    this.initSortingVisual(sortParams);
+    const sortDropdown = document.querySelector('.sort-dropdown__label');
     const priceAsc = document.getElementById('price-asc');
     const priceDes = document.getElementById('price-des');
     const ratingAsc = document.getElementById('rating-asc');
@@ -195,7 +280,8 @@ class AppView {
         if (a.price < b.price) return 1;
         return 0;
       });
-      this.addToQuery('priceAsc', sortParams);
+      if (sortDropdown) sortDropdown.textContent = 'Price descending';
+      this.addToQuery('priceDes', sortParams);
       this.createSortedCatalog(filteredArr);
     });
     priceDes?.addEventListener('click', () => {
@@ -204,7 +290,8 @@ class AppView {
         if (a.price > b.price) return 1;
         return 0;
       });
-      this.addToQuery('priceDes', sortParams);
+      if (sortDropdown) sortDropdown.textContent = 'Price ascending';
+      this.addToQuery('priceAsc', sortParams);
       this.createSortedCatalog(filteredArr);
     });
     ratingAsc?.addEventListener('click', () => {
@@ -213,7 +300,8 @@ class AppView {
         if (a.rating < b.rating) return 1;
         return 0;
       });
-      this.addToQuery('ratingAsc', sortParams);
+      if (sortDropdown) sortDropdown.textContent = 'Rating descending';
+      this.addToQuery('ratingDes', sortParams);
       this.createSortedCatalog(filteredArr);
     });
     ratingDes?.addEventListener('click', () => {
@@ -222,10 +310,26 @@ class AppView {
         if (a.rating > b.rating) return 1;
         return 0;
       });
-      this.addToQuery('ratingDes', sortParams);
+      if (sortDropdown) sortDropdown.textContent = 'Rating ascending';
+      this.addToQuery('ratingAsc', sortParams);
       this.createSortedCatalog(filteredArr);
     });
   }
+
+  initSortingVisual(sortParams: URLSearchParams) {
+    
+    const sortDropdown = document.querySelector('.sort-dropdown__label');
+    const sortParamsNew = sortParams.get('sort');
+    console.log(sortParamsNew);
+    if (sortDropdown) {
+      if (sortParamsNew == 'priceAsc') sortDropdown.textContent = 'Price ascending';
+      else if (sortParamsNew == 'priceDes') sortDropdown.textContent = 'Price descending';
+      else if (sortParamsNew == 'ratingAsc') sortDropdown.textContent = 'Rating ascending';
+      else if (sortParamsNew == 'ratingDes') sortDropdown.textContent = 'Rating descending';
+    }
+  }
+
+  // Query methods
 
   addToQuery(type: string, sortParams: URLSearchParams) {
     sortParams.set('sort', type);
@@ -262,53 +366,7 @@ class AppView {
     }
   }
 
-  createPages(filteredArr: Types.Product[], pagesCount: number) {
-    const pagesWrapper = document.querySelector('.pages-wrapper');
-    const pageNext = document.querySelector('.page-next');
-    const pagePrev = document.querySelector('.page-prev');
 
-    if (pagesWrapper) {
-      pagesWrapper.innerHTML = '';
-    }
-    const pagesArr: HTMLDivElement[] = [];
-    for (let i = 0; i < pagesCount; i++) {
-      pagesArr.push(document.createElement('p'));
-      pagesArr[i].className = 'catalog__page';
-      pagesArr[i].className = 'page-idx';
-      pagesArr[i].textContent = String(i + 1);
-      pagesWrapper?.append(pagesArr[i]);
-      pagesArr[i].addEventListener('click', () => {
-        this.goToPage(filteredArr, pagesArr, i);
-      });
-    }
-    pagesArr[0].classList.add('page-idx--active');
-    pageNext?.addEventListener('click', () => {
-      this.goToPage(filteredArr, pagesArr, this.findPageIdx(pagesArr) + 1);
-    });
-    pagePrev?.addEventListener('click', () => {
-      this.goToPage(filteredArr, pagesArr, this.findPageIdx(pagesArr) - 1);
-    });
-  }
-
-  goToPage(filteredArr: Types.Product[], pagesArr: HTMLDivElement[], idx: number) {
-    const catalogDiv: HTMLDivElement | null = document.querySelector('.cards-wrapper');
-    if (idx >= 0 && idx < pagesArr.length) {
-      for (let i = 0; i < pagesArr.length; i++) {
-        pagesArr[i].classList.remove('page-idx--active');
-      }
-      pagesArr[idx].classList.add('page-idx--active');
-      if (catalogDiv) {
-        this.createCatalog(filteredArr, catalogDiv, idx);
-      }
-    }
-  }
-
-  findPageIdx(pagesArr: HTMLDivElement[]) {
-    for (let i = 0; i < pagesArr.length; i++) {
-      if (pagesArr[i].classList.contains('page-idx--active')) return i;
-    }
-    return 0;
-  }
 
   createCart() {
     this.cart.initCartPage();
